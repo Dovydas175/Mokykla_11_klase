69b5e12b0745f4192b143fd68338168b4995f808
Dovydas Žiedelis <116582984+Dovydas175@users.noreply.github.com>
2022-10-27 08:41:19 +0300

    Add files via upload

---
+++ 22-10-27/main.cpp
@@ -0,0 +1,32 @@
+//Dovydas Þiedelis IIIA
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int A[30];
+    int suma;
+    double vidmt, vidmen;
+    ifstream fd("Duomenys.txt");
+    fd >> n;
+    fro(int i = 0; i < n; i++)
+    {
+        fd << A[i] << endl;
+    }
+    fd.close();
+    suma = 0;
+    for (int i = 0; i < n; i++)
+    {
+        suma = suma + A[i];
+    }
+    vidmt = suma / n;
+
+    ofstream fr("Rezultatai.txt");
+    fr << suma << endl;
+    fr << vidmt << endl;
+
+    fr.close();
+    return 0;
+}
